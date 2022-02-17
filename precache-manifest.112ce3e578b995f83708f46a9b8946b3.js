8561e4f0cc71c8a46a4b59c6da77a825c0fee9e6
Inhoi Heo <inhoi.heo@oecd.org>
2022-02-17 17:07:00 +0100

    Updates

---
+++ precache-manifest.112ce3e578b995f83708f46a9b8946b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90e9e638c926ab16036fa65b5ba3a16",
+    "url": "/pacdigitalstory/index.html"
+  },
+  {
+    "revision": "ea1611c7a873cce42a12",
+    "url": "/pacdigitalstory/static/css/main.c4a6ccf3.chunk.css"
+  },
+  {
+    "revision": "3997b25983866289e48e",
+    "url": "/pacdigitalstory/static/js/2.0506049c.chunk.js"
+  },
+  {
+    "revision": "76a71f0fee5a38c30a1ca9ad56706565",
+    "url": "/pacdigitalstory/static/js/2.0506049c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1611c7a873cce42a12",
+    "url": "/pacdigitalstory/static/js/main.4acd9a1f.chunk.js"
+  },
+  {
+    "revision": "e3d97833849816c5c29e",
+    "url": "/pacdigitalstory/static/js/runtime-main.fa06d84d.js"
+  }
+]);
